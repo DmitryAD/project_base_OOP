bc2aa91fba2b60b41ebe91e6ca181bf1db71a7c4
DmitryAD <dima-androsenko1@mail.ru>
2026-09-08 16:54:37 +0300

    Update exceptions.py

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,1 +1,18 @@
+class AccountFrozenError(Exception):
+    """Операция запрещена: счёт заморожен."""
+    pass
 
+
+class AccountClosedError(Exception):
+    """Операция запрещена: счёт закрыт."""
+    pass
+
+
+class InvalidOperationError(Exception):
+    """Некорректная операция (например, неверная сумма)."""
+    pass
+
+
+class InsufficientFundsError(Exception):
+    """Недостаточно средств для снятия."""
+    pass
